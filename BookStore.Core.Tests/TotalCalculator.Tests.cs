90c423ec51e0ab884d675e8d824afde81aa63d4a
Daniel de Souza Cacheta <daniel.cacheta@programmers.com.br>
2018-06-19 10:12:40 -0300

    Calculate discount based on number of books purchased

---
+++ BookStore.Core.Tests/TotalCalculator.Tests.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BookStore.Core.Tests
+{
+    [TestClass]
+    public class TotalCalculatorTests
+    {
+        [DataTestMethod]
+        [DataRow(1, 42)]
+        [DataRow(2, 79.80)]
+        [DataRow(3, 113.40)]
+        [DataRow(4, 142.80)]
+        [DataRow(5, 168)]
+        [DataRow(8, 268.80)]
+        public void MustCalculateTotalBasedOnNumberOfBooks(int howManyBooks, double expectedTotal)
+        {
+            var sut = new TotalCalculator();
+            var result = sut.CalculateTotal(howManyBooks);
+
+            Assert.AreEqual(expectedTotal, result);
+        }
+        
+    }
+}
---
+++ BookStore.Core/TotalCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BookStore.Core
+{
+    public class TotalCalculator
+    {
+        private const int UNIT_PRICE = 42;
+
+        public double CalculateTotal(int howManyBooks)
+        {
+            var discount = CalculateDiscountRate(howManyBooks);
+            return (howManyBooks * UNIT_PRICE) - (discount * (howManyBooks * UNIT_PRICE));
+        }
+
+        private double CalculateDiscountRate(int howManyBooks)
+        {
+            if (howManyBooks >= 5) return 0.20;
+            if (howManyBooks == 4) return 0.15;
+            if (howManyBooks == 3) return 0.10;
+            if (howManyBooks == 2) return 0.05;
+            return 0;
+        }
+    }
+}
